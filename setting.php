03d154d360cae1be5eeb71449f71998107b36dd5
Luca Mainieri <luca.mainieri@gmail.com>
2016-09-13 10:17:57 +0200

    Update setting.php

--- setting.php
+++ setting.php
@@ -1,27 +1,30 @@
 <?php
+
 /********************************************************************************/
 /********************************************************************************/
-//      CONFIGURAZIONE // SETTING 
+//      CHECKER SETTING 
 /********************************************************************************/
 /********************************************************************************/
 
 // path site root to check integrity (indicare parent directory - percorso assoluto del server)
-define("PATH", "/home/neting/public_html/");
+define("PATH", "/home/user/public_html/");
 
 
-// check file - file di controllo e verifica ( TENERE FUORI DAL PERCORSO PUBBLICO !!!)
+// check file - file di controllo e verifica ( keep this file out of public accessible directory )
+// this file will save the domain configuration data for comparision
 $checkFile = "check.txt";
 
-// file logs of warning ( TENERE FUORI DAL PERCORSO PUBBLICO !!!)
+// file logs of warning ( keep this file out of public accessible directory )
+// this file will logs the scan resulting for further usage
 $logFile = "logs.txt";
 
 //mail setting for warnings
-$emailAddress = "supporto@neting.it";
-$emailSubject = "Controllo integrità sito - NETING.IT - ".PATH;
+$emailAddress = "your@mail.com";
+$emailSubject = "Check integrity at - ".PATH;
 
 //decide whether you want to get an email with all debug info of scanning
 $sendDebugEmail = true;
-$debugEmailAddress = 'l.mainieri@neting.it';
+$debugEmailAddress = 'your@mail.com';
 
 
 // file with extensions to exclude from control, you can point this to a remote file
